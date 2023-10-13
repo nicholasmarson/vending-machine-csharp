643f7abbfa8d6de8a8826e9fe90b24490e598c6d
Nicholas Marson <nicholasmarson@Nicholass-MBP.attlocal.net>
2023-10-12 21:42:43 -0400

    Pushing up account manager changes

--- VendingMachineCLI/VendingMachineCLI/Accounting/AccountManager.cs
+++ VendingMachineCLI/VendingMachineCLI/Accounting/AccountManager.cs
@@ -14,26 +14,56 @@ namespace VendingMachineCLI.Accounting
         private int dime;
         private int nickel;
         private int changeInCents;
-        private double depositAmount;
+        private decimal depositAmount;
 
         public decimal Balance { get => balance; set => balance = value; }
-        public double DepositAmount { get => depositAmount; set => depositAmount = value; }
-
-
+        public decimal DepositAmount { get => depositAmount; set => depositAmount = value; }
 
         public void feedMoney()
         {
-
+            Console.WriteLine();
+            Console.WriteLine("How much money would you like to deposit?");
+            Console.WriteLine();
+            if (decimal.TryParse(Console.ReadLine(), out depositAmount))
+            {
+                balance = depositAmount + balance;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please enter a number.");
+            }
+            Console.WriteLine("Your current balance is now $" + balance);
         }
 
         public void deductFromBalance()
         {
-
+            
         }
 
         public void dispenseChange()
         {
+            int changeInSmallestCents = (int) (balance * 100);
+            quarter = changeInSmallestCents % 25;
+            dime = changeInSmallestCents % 10;
+            nickel = changeInSmallestCents % 5;
+            double changeInDollars = changeInSmallestCents / 100.0;
 
+            Console.WriteLine();
+            Console.WriteLine("Your change is: " + balance + " in " + quarter + "quarters, " + nickel + "nickels, and " + dime + " dimes");
+            Console.WriteLine();
+            balance = 0;
+            Console.WriteLine();
+            Console.WriteLine("Your remaining balance is: " + balance);
+            Console.WriteLine();
+
+        }
+
+
+        public void applyBuyOneGetOneDollarOffDiscount()
+        {
+            Console.WriteLine("It's BOGODO for August! You get $1 off!");
+            Console.WriteLine();
+            balance += 1;
         }
     }
 }
